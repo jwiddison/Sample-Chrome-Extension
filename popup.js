925d2fc2c6d427a8d24ef9dc620206a0e85f1976
Jordan Widdison <jordan.widdison@saharadev.com>
2017-03-31 15:28:31 -0600

    Add random generator for different quotes each time.

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
+    var id = Math.floor((Math.random() * 100) + 1);
     $.ajax({
-        url: "https://sethquotesapi.herokuapp.com/quotes/2"
+        url: "https://sethquotesapi.herokuapp.com/quotes/" + id
     }).then(function(data) {
        $('.quote').html('"' + data.text + '"');
     });
-
 });
